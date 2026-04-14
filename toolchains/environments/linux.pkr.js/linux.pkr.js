477734f44d0b123f1ce02312d804bd717aaf6c6d
luc lambour <luc@tipi.build>
2026-04-14 11:48:28 +0200

    🔧 add toolchain
    add cmake-re instruction

---
+++ toolchains/environments/linux.pkr.js/linux.pkr.js
@@ -0,0 +1,10 @@
+{
+  "variables": { },
+  "builders": [
+    {
+      "type": "docker",
+      "image": "nilurova68440/tipi-ubuntu2404-paraview@sha256:803c75e02eb0763492431bd58ede338193dfab3fb5859f9df7c0653d0a708a70",
+      "commit": true
+    }
+  ]
+}
